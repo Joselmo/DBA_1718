c29a25fb08e39fc06a11d1e95ce381d7e7f16603
Jose <joselmo@correo.ugr.es>
2017-10-21 17:34:25 +0200

    Cambios tontos para test

--- GugelCar/src/PruebaClass.java
+++ GugelCar/src/PruebaClass.java
@@ -2,5 +2,5 @@ public class PruebaClass {
 
     int hola = 0;
     String ha = "Esto es una prueba de un commit -Diego, y esto es para forzar a que haya conflicto";
-    String asdf = "asdf";
+    String asdf = "asdfaasdasdadsa";
 }
